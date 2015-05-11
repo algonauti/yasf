4dbf34c99af321f7df7b3a3764d398a8c926dbc3
Davide Papagni <davide@algonauti.com>
2015-05-11 17:21:47 +0200

    update yasf spec after wowebook page title change

--- spec/lib/yasf_spec.rb
+++ spec/lib/yasf_spec.rb
@@ -12,7 +12,7 @@ describe Yasf do
       Given(:scraper) { Fakecrawler.new }
       When(:result) { scraper.crawl }
 
-      Then { expect(result.page_title).to eql('Home | Wow! eBook') }
+      Then { expect(result.page_title).to eql('Wow! eBook › The best eBook site ever!') }
       And { expect(result.books.count).to be(6) }
       And { expect(result.books.last.title).to eql('FIVE STARS: PUTTING ONLINE REVIEWS TO WORK FOR YOUR BUSINESS') }
     end
