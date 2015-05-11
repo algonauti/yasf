739305f1b8d611f1debb71f6976823db996f8308
Davide Papagni <davide@algonauti.com>
2015-05-11 18:03:42 +0200

    Monkey patch for eventmachine_httpserver

--- spec/support/billy.rb
+++ spec/support/billy.rb
@@ -4,3 +4,16 @@ Billy.configure do |c|
 end
 
 Capybara.default_driver = :poltergeist_billy
+
+# Monkey patch for eventmachine_httpserver
+module EventMachine
+	class HttpResponse
+		def initialize
+			super
+      @keep_connection_open = true
+      @sent_content = false
+      @sent_headers = false
+		end
+  end
+end
+
