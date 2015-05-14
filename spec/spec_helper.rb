2f1813d0f64852f0591d2d3fc9621130789f8dd2
Salvatore Ferrucci <salvatore@algonauti.com>
2015-05-14 09:15:02 +0200

    configure yasf before to load all support classes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,14 +9,20 @@ Dotenv.load('.env')
 
 require 'yasf'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
 RSpec.configure do |config|
+  config.before :suite do
+    Yasf.configure do |config|
+      config.timeout = 60
+    end
+  end
+
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = :random
   config.default_formatter = 'doc' if config.files_to_run.one?
   config.warnings = true
 end
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
